5e846a5c8a4229a4c28717f9ac934eece3dbaacc
nikhi upadhyay <nikhil.upadhyay_cs19@gla.ac.in>
2021-02-19 16:24:53 +0530

    complete implimentation of stack using linked list

--- src/implimentation/main/MyMain.java
+++ src/implimentation/main/MyMain.java
@@ -6,6 +6,19 @@ public class MyMain {
 
     public static void main(String[] args) {
         MyStack stk=new MyStack();
+        System.out.println(stk.isEmpty());
+        stk.push(5);
+        stk.push(10);
+        stk.push(15);
+        stk.push(20);
+        stk.push(25);
+        System.out.println(stk.size());
+        System.out.println(stk.isEmpty());
+        System.out.println(stk.peek());
+        System.out.println(stk.pop());
+        System.out.println(stk.size());
+        stk.traverse();
+        System.out.println(stk.sizeByTraverse());
 
     }
 }
